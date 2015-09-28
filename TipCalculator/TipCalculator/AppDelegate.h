54a5435c39514dfe97b0acbf0d16410432fd155b
alexlester <alex.d.j.lester@gmail.com>
2015-09-27 22:56:28 -0700

    Full commit for basic functionality

---
+++ TipCalculator/TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Alex Lester on 27/09/2015.
+//  Copyright (c) 2015 Alex Lester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
