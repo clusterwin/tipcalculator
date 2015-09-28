7b9ce6ecb30f20d46d84afd429e699d4b05a9ef2
alexlester <alex.d.j.lester@gmail.com>
2015-09-27 20:40:55 -0700

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Alex Lester on 27/09/2015.
+//  Copyright (c) 2015 Alex Lester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TipCalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TipCalculator
+//
+//  Created by Alex Lester on 27/09/2015.
+//  Copyright (c) 2015 Alex Lester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
