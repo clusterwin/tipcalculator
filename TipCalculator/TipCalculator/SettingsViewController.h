84db84a53ed6d947830d0d2ce81c78c486c78805
alexlester <alex.d.j.lester@gmail.com>
2015-09-30 18:04:27 -0700

    Add in settings tab access

---
+++ TipCalculator/TipCalculator/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  TipCalculator
+//
+//  Created by Alex Lester on 30/09/2015.
+//  Copyright (c) 2015 Alex Lester. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
